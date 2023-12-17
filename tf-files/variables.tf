4180b83bd816a1958dc5f6bfc93f7a74499e816f
Cloud-Techno <ferdiates157@gmail.com>
2023-12-17 01:10:03 -0500

    uploaded terraform files

---
+++ tf-files/variables.tf
@@ -0,0 +1,16 @@
+variable "git-name" {
+  default = "Cloud-Techno"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "ates"
+}
+
+variable "hosted-zone" {
+  default = "swissdevcloud.com"
+}
+
